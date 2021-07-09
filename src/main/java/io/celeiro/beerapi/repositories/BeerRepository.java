b03b16d32326e0b2c8b4e2a426c71e8b15fe98f7
Geuel Chaves Paiva <geuelcp@gmail.com>
2021-07-09 12:42:59 -0300

    findByName BeerRrepository

--- src/main/java/io/celeiro/beerapi/repositories/BeerRepository.java
+++ src/main/java/io/celeiro/beerapi/repositories/BeerRepository.java
@@ -3,5 +3,9 @@ package io.celeiro.beerapi.repositories;
 import io.celeiro.beerapi.entities.Beer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface BeerRepository extends JpaRepository<Beer, Long> {
+
+    Optional<Beer> findByName(String name);
 }
