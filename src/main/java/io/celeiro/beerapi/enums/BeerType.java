67488c08d2ed604323cf9c2bc579211e038327b1
Geuel Chaves Paiva <geuelcp@gmail.com>
2021-07-09 12:27:34 -0300

    Entity beer and Enum BeerType

---
+++ src/main/java/io/celeiro/beerapi/enums/BeerType.java
@@ -0,0 +1,19 @@
+package io.celeiro.beerapi.enums;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum BeerType {
+
+    LAGER("Lager"),
+    MALZBIER("Malzbier"),
+    WITBIER("Witbier"),
+    WEISS("Weiss"),
+    ALE("Ale"),
+    IPA("Ipa"),
+    STOUT("Stout");
+
+    private final String description;
+}
